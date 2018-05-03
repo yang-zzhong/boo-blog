44a70f736eb2cae69644e3e3603f85c0ee33a825
hackyoung <hackyoung@163.com>
2018-05-03 18:35:41 +0800

    路由修改

--- http/route/router.go
+++ http/route/router.go
@@ -16,7 +16,45 @@ func Router(docRoot string) *httprouter.Router {
 	return router
 }
 
-func registerRoute(router *httprouter.Router) {
+func registerNeedAuthRoute(router *httprouter.Router) {
+	router.Post("/blogs", func(w ResponseWriter, req *httprouter.Request, p *P) {
+		blog := &controller.Article{controller.NewController(w)}
+		blog.Create(req, p)
+	})
+	router.Put("/blogs/:id", func(w ResponseWriter, req *httprouter.Request, p *P) {
+		blog := &controller.Article{controller.NewController(w)}
+		blog.Update(req, p)
+	})
+	router.Delete("/blogs/:id", func(w ResponseWriter, req *httprouter.Request, p *P) {
+		blog := &controller.Article{controller.NewController(w)}
+		blog.Remove(req, p)
+	})
+	router.Post("/images", func(w ResponseWriter, req *httprouter.Request, p *P) {
+		image := &controller.Image{controller.NewController(w)}
+		image.Create(req, p)
+	})
+	router.Post("/tags", func(w ResponseWriter, req *httprouter.Request, p *P) {
+		tag := &controller.Tag{controller.NewController(w)}
+		tag.Create(req, p)
+	})
+	router.Post("/cates", func(w ResponseWriter, req *httprouter.Request, p *P) {
+		cate := &controller.Category{controller.NewController(w)}
+		cate.Create(req, p)
+	})
+	router.Put("/cates/:id", func(w ResponseWriter, req *httprouter.Request, p *P) {
+		cate := &controller.Category{controller.NewController(w)}
+		cate.Update(req, p)
+	})
+	router.Delete("/cates/:id", func(w ResponseWriter, req *httprouter.Request, p *P) {
+		cate := &controller.Category{controller.NewController(w)}
+		cate.Delete(req, p)
+	})
+}
+
+func registerPublicRoute(router *httprouter.Router) {
+	router.Get("/hello-world", func(w ResponseWriter, req *httprouter.Request, _ *P) {
+		io.WriteString(w, "hello world")
+	})
 	router.Post("/register", func(w ResponseWriter, req *httprouter.Request, _ *P) {
 		login := &controller.Login{controller.NewController(w)}
 		login.Register(req)
@@ -32,9 +70,6 @@ func registerRoute(router *httprouter.Router) {
 	router.Get("/users/:name", func(w ResponseWriter, req *httprouter.Request, p *P) {
 		user := &controller.User{controller.NewController(w)}
 		user.One(req, p)
-	})
-	router.Get("/hello-world", func(w ResponseWriter, req *httprouter.Request, _ *P) {
-		io.WriteString(w, "hello world")
 	})
 	router.Get("/images", func(w ResponseWriter, req *httprouter.Request, _ *P) {
 		image := &controller.Image{controller.NewController(w)}
@@ -57,39 +92,14 @@ func registerRoute(router *httprouter.Router) {
 		blog.Find(req)
 	})
 	router.Get("/cates", func(w ResponseWriter, req *httprouter.Request, _ *P) {
-		blog := &controller.Category{controller.NewController(w)}
-		blog.Find(req)
+		cate := &controller.Category{controller.NewController(w)}
+		cate.Find(req)
 	})
+}
+
+func registerRoute(router *httprouter.Router) {
+	registerPublicRoute(router)
 	ms := httprouter.NewMs()
 	ms.Append(middleware.AuthUser)
-	router.Group("", ms, func(router *httprouter.Router) {
-		router.Post("/blogs", func(w ResponseWriter, req *httprouter.Request, p *P) {
-			blog := &controller.Article{controller.NewController(w)}
-			blog.Create(req, p)
-		})
-		router.Put("/blogs/:id", func(w ResponseWriter, req *httprouter.Request, p *P) {
-			blog := &controller.Article{controller.NewController(w)}
-			blog.Update(req, p)
-		})
-		router.Delete("/blogs/:id", func(w ResponseWriter, req *httprouter.Request, p *P) {
-			blog := &controller.Article{controller.NewController(w)}
-			blog.Remove(req, p)
-		})
-		router.Post("/images", func(w ResponseWriter, req *httprouter.Request, p *P) {
-			image := &controller.Image{controller.NewController(w)}
-			image.Create(req, p)
-		})
-		router.Post("/tags", func(w ResponseWriter, req *httprouter.Request, p *P) {
-			tag := &controller.Tag{controller.NewController(w)}
-			tag.Create(req, p)
-		})
-		router.Post("/cates", func(w ResponseWriter, req *httprouter.Request, p *P) {
-			cate := &controller.Category{controller.NewController(w)}
-			cate.Create(req, p)
-		})
-		router.Put("/cates", func(w ResponseWriter, req *httprouter.Request, p *P) {
-			cate := &controller.Category{controller.NewController(w)}
-			cate.Update(req, p)
-		})
-	})
+	router.Group("", ms, registerNeedAuthRoute)
 }
